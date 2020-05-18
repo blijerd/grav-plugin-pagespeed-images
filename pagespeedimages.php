9125922ccc5be533728dd1b50834fa70383149dd
edwinrasser <edwin@blijwin.nl>
2020-05-18 20:20:32 +0200

    js fix

--- pagespeedimages.php
+++ pagespeedimages.php
@@ -44,8 +44,7 @@ class PagespeedImagesPlugin extends Plugin
     {
         return [
             'onPageContentProcessed' => ['onPageContentProcessed', 0],
-            'onPluginsInitialized' => ['onPluginsInitialized', 0],
-            'onImageMediumSaved' => ['onImageMediumSaved', 0]
+            'onPluginsInitialized' => ['onPluginsInitialized', 0]
         ];
     }
 
