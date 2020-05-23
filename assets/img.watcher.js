450b316a5bdf1ef4b4f51919b81a14553d0379f1
edwinrasser <edwin@blijwin.nl>
2020-05-23 13:15:53 +0200

    webp support off

--- assets/img.watcher.js
+++ assets/img.watcher.js
@@ -1,24 +1,5 @@
-function testWebP(callback) {
-    var webP = new Image();
-    webP.onload = webP.onerror = function () {
-        callback(webP.height == 2);
-    };
-    webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
-};
-var webpSupport;
 function lazyImages() {
-
-    testWebP(function (support) {
-        var body = document.body;
-        if (support) {
-            body.classList.add("webp");
-            webpSupport = true;
-        } else {
-            body.classList.add("nowebp");
-        }
-    });
-
-
+    console.log('init lazy');
     var lazyImages = [].slice.call(document.querySelectorAll('[data-lazysrc]'));
 
     if ("IntersectionObserver" in window) {
@@ -27,21 +8,21 @@ function lazyImages() {
                 if (entry.isIntersecting) {
                     let lazyImage = entry.target;
 
-                    if (webpSupport == true && lazyImage.dataset.srcwebp) {
-                        lazyImage.src = lazyImage.dataset.srcwebp;
-                        // console.log('loading ' + lazyImage.dataset.srcwebp);
-                        if (lazyImage.dataset.srcsetwebp) {
-                            lazyImage.srcset = lazyImage.dataset.srcsetwebp;
-                            // console.log('srcset webp found');
-                        }
-                    } else {
+                    // if (webpSupport == true && lazyImage.dataset.srcwebp) {
+                    //     lazyImage.src = lazyImage.dataset.srcwebp;
+                    //     // console.log('loading ' + lazyImage.dataset.srcwebp);
+                    //     if (lazyImage.dataset.srcsetwebp) {
+                    //         lazyImage.srcset = lazyImage.dataset.srcsetwebp;
+                    //         // console.log('srcset webp found');
+                    //     }
+                    // } else {
                         lazyImage.src = lazyImage.dataset.lazysrc;
-                        // console.log('loading ' + lazyImage.dataset.src);
-                        if (lazyImage.dataset.srcset) {
-                            lazyImage.srcset = lazyImage.dataset.srcset;
-                            // console.log('srcset found');
-                        }
-                    }
+                    //     // console.log('loading ' + lazyImage.dataset.src);
+                    //     if (lazyImage.dataset.srcset) {
+                    //         lazyImage.srcset = lazyImage.dataset.srcset;
+                    //         // console.log('srcset found');
+                    //     }
+                    // }
                     lazyImageObserver.unobserve(lazyImage);
                 }
             });
@@ -52,6 +33,7 @@ function lazyImages() {
         });
     }
 }
+
 lazyImages();
 document.addEventListener("DOMContentLoaded", function () {
     lazyImages();
