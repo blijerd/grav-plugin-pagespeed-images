cad85eea04dda53035560e9767f34029862728a9
edwinrasser <edwin@blijwin.nl>
2020-05-23 15:06:22 +0200

    undow ithout let

--- assets/img.watcher.js
+++ assets/img.watcher.js
@@ -3,7 +3,7 @@ function lazyImages() {
     var lazyImages = [].slice.call(document.querySelectorAll('[data-lazysrc]'));
 
     if ("IntersectionObserver" in window) {
-        lazyImageObserver = new IntersectionObserver(function (entries, observer) {
+        let lazyImageObserver = new IntersectionObserver(function (entries, observer) {
             entries.forEach(function (entry) {
                 if (entry.isIntersecting) {
                     let lazyImage = entry.target;
